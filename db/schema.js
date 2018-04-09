ca61f0a8ca091cbb7ca3c0bbbbbbdbfb62f75927
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-09 10:57:24 -0400

    getting things warm

--- db/schema.js
+++ db/schema.js
@@ -2,9 +2,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const User = new Schema({
-    username,
-    text,
-    first_name,
-    last_name,
+    username: String,
+    email: String,
+    first_name: String,
+    last_name: String,
+    photo_url: String,
+    stores: [Store]
 
+})
+const Store = new Schema({
+    name: String,
+    address: String,
+    gifts: [Gift]
+})
+const Gift = new Schema({
+    name: String,
+    description: String,
+    price: Number,
+    gift_giver: String
 })
