72827fe17f813c1ac20d6fbfa96fcf7dc05ec322
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-09 11:39:33 -0400

    i think i am done refactoring

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 
 const app = express()
-
-
+app.set("view engine", "hbs")
 app.listen(3000, () => {
     console.log("getting turnt on port 3000")
 })
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
-res.redirect("/users", (req, res) => {
-    res.send("Users Page")
+app.get("/users", (req, res) => {
+    res.render("users/index", {})
 })
