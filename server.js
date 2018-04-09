8387e59ea26f0f5e04e50e39342bb297698dee77
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-09 14:08:36 -0400

    connected to mongodb

--- server.js
+++ server.js
@@ -2,15 +2,42 @@ const express = require('express')
 const hbs = require('hbs')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose')
 const app = express()
 app.set("view engine", "hbs")
+
 app.listen(3000, () => {
     console.log("getting turnt on port 3000")
+
+})
+mongoose.connect("mongodb://localhost/regifter")
+
+mongoose.connection.once('open', () => {
+    console.log(`Mongoose has connected to MongoDB`)
+})
+
+mongoose.connection.on('error', (error) => {
+    console.error(`
+    MongoDB connection error!!! 
+    ${error}
+  `)
+    process.exit(-1)
 })
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
+const User = require("./models/User")
 app.get("/users", (req, res) => {
-    res.render("users/index", {})
+    console.log("hitting users route")
+    User.find({})
+        .then((users) => {
+            console.log("USERS", users)
+            res.render("users/index", {
+                users
+            })
+
+        })
+
+
+
 })
