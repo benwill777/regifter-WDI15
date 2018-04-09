fac558d76ebe5a8c5f43f5d65525afc1dd28ab10
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-09 15:13:27 -0400

    linked seeds

--- db/seeds.js
+++ db/seeds.js
@@ -12,6 +12,11 @@ app.listen(PORT, () => {
 mongoose.connection.once('open', () => {
     console.log(`Mongoose has connected to MongoDB`)
 })
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect("mongodb://localhost/regifter")
+}
 
 mongoose.connection.on('error', (error) => {
     console.error(`
