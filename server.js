03dac98c47ac178bbca843ed8243c6ebe712c4f4
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-09 11:17:33 -0400

    added root/users and added layout.hbs

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const hbs = require('hbs')
-const methodOverride = require('method-overeride')
+const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 
 const app = express()
+
+
+app.listen(3000, () => {
+    console.log("getting turnt on port 3000")
+})
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+res.redirect("/users", (req, res) => {
+    res.send("Users Page")
+})
